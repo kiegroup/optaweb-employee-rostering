bd0f49cf68ed724deae28e61b63523cda1b111b7
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-05-09 16:40:08 +0200

    Add delete buttons in grid

--- optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/skill/SkillListPanel.java
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/skill/SkillListPanel.java
@@ -3,15 +3,21 @@ package org.optaplanner.openshift.employeerostering.gwtui.client.skill;
 import java.util.List;
 
 import com.github.nmorel.gwtjackson.rest.api.RestCallback;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
 import org.gwtbootstrap3.client.ui.Pagination;
+import org.gwtbootstrap3.client.ui.constants.ButtonType;
+import org.gwtbootstrap3.client.ui.constants.IconType;
+import org.gwtbootstrap3.client.ui.gwt.ButtonCell;
 import org.gwtbootstrap3.client.ui.gwt.CellTable;
 import org.optaplanner.openshift.employeerostering.shared.domain.Roster;
 import org.optaplanner.openshift.employeerostering.shared.rest.RosterRestServiceBuilder;
@@ -22,6 +28,8 @@ public class SkillListPanel extends Composite {
 
     interface SkillListUiBinder extends UiBinder<Widget, SkillListPanel> {}
     private static final SkillListUiBinder uiBinder = GWT.create(SkillListUiBinder.class);
+
+    private Long tenantId = -1L;
 
     // TODO use DataGrid instead
     @UiField(provided = true)
@@ -52,14 +60,27 @@ public class SkillListPanel extends Composite {
             }
         }, "Name");
 
-//        final Column<Employee, String> col4 = new Column<Employee, String>(new ButtonCell(ButtonType.PRIMARY, IconType.GITHUB)) {
-//            @Override
-//            public String getValue(Employee object) {
-//                return "Click Me";
-//            }
-//        };
-//        col4.setFieldUpdater((index, object, value) -> Window.alert("Clicked!"));
-//        rosterTable.addColumn(col4, "Buttons");
+        Column<Skill, String> deleteColumn = new Column<Skill, String>(new ButtonCell(ButtonType.DANGER, IconType.REMOVE)) {
+            @Override
+            public String getValue(Skill skill) {
+                return "Delete";
+            }
+        };
+        deleteColumn.setFieldUpdater((index, skill, value) -> {
+            SkillRestServiceBuilder.removeSkill(tenantId, skill, new RestCallback<Boolean>() {
+                @Override
+                public void onSuccess(Boolean removed) {
+                    refreshData();
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    Window.alert("Failure calling REST method: " + throwable.getMessage());
+                    throw new IllegalStateException("REST call failure", throwable);
+                }
+            });
+        });
+        table.addColumn(deleteColumn, "Actions");
 
         table.addRangeChangeHandler(event -> pagination.rebuild(pager));
 
@@ -69,7 +90,6 @@ public class SkillListPanel extends Composite {
     }
 
     protected void refreshData() {
-        Long tenantId = -1L;
         SkillRestServiceBuilder.getSkillList(tenantId, new RestCallback<List<Skill>>() {
             @Override
             public void onSuccess(List<Skill> skillList) {
@@ -80,6 +100,7 @@ public class SkillListPanel extends Composite {
 
             @Override
             public void onFailure(Throwable throwable) {
+                Window.alert("Failure calling REST method: " + throwable.getMessage());
                 throw new IllegalStateException("REST call failure", throwable);
             }
         });
