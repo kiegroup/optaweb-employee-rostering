c14001ff4f24d800f6c5edec4bad64e66d9e47e5
Christopher James Chianelli <cchianel@redhat.com>
2017-09-25 14:17:36 -0400

    [PLANNER-889] Server's Error Stack Trace is shown in a Error Popup on Client

---
+++ optashift-employee-rostering-server/src/main/java/org/optaplanner/openshift/employeerostering/server/exceptions/OptashiftExceptionMapper.java
@@ -0,0 +1,24 @@
+package org.optaplanner.openshift.employeerostering.server.exceptions;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class OptashiftExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception exception) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(exception.getMessage());
+        for (StackTraceElement trace : exception.getStackTrace()) {
+            builder.append(trace.toString()).append('\n');
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .type(MediaType.TEXT_PLAIN)
+                .entity(builder.toString())
+                .build();
+    }
+
+}
