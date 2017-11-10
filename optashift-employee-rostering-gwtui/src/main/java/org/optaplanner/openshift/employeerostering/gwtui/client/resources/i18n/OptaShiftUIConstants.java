1ad608c57f6de6f351b67d657ff2c9b799ed0fa6
Christopher James Chianelli <cchianel@redhat.com>
2017-11-10 09:29:09 -0500

    Sorted/Formatted i18n constants, fixed a bug in WannabeSolverManager

--- optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/resources/i18n/OptaShiftUIConstants.java
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/resources/i18n/OptaShiftUIConstants.java
@@ -3,90 +3,91 @@ package org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n;
 import org.jboss.errai.ui.shared.api.annotations.TranslationKey;
 
 public interface OptaShiftUIConstants {
-	
-	@TranslationKey(defaultValue="Actions")
-    final String General_actions = "General.actions";
-	
-	@TranslationKey(defaultValue="Add")
-	final String General_add = "General.add";
-	
-	@TranslationKey(defaultValue="AbstractRosterViewPanel.startSolving")
-	final String AbstractRosterViewPanel_startSolving = "Click the <i>Solve</i> button to start solving.";
-	
-	@TranslationKey(defaultValue="AbstractRosterViewPanel.terminateEarly")
-	final String AbstractRosterViewPanel_terminateEarly = "Terminate";
-	
-	@TranslationKey(defaultValue="Delete")
-    final String General_delete = "General.delete";
-	
-	
-	
-	@TranslationKey(defaultValue="Employee")
-    final String General_employee = "General.employee";
-	
-	@TranslationKey(defaultValue="Employee name")
-	final String EmployeeListPanel_employeeName = "EmployeeListPanel.employeeName";
-	
-	@TranslationKey(defaultValue="Employee Roster")
-    final String MenuPanel_employeeRoster = "MenuPanel.employeeRoster";
-	
-	@TranslationKey(defaultValue="Employees")
-    final String MenuPanel_employees = "MenuPanel.employees";
-	
-	
-	@TranslationKey(defaultValue="The server returned an empty result which is impossible.\nMaybe the server is using the wrong WildFly version. Try using WildFly 10.1.0.Final.")
+
+    @TranslationKey(defaultValue="The server returned an empty result which is impossible.\nMaybe the server is using the wrong WildFly version. Try using WildFly 10.1.0.Final.")
     final String AbstractRosterViewPanel_emptyResult = "AbstractRosterViewPanel.emptyResult";
-	
-	@TranslationKey(defaultValue="Finished solving")
+
+    @TranslationKey(defaultValue="Finished solving")
     final String AbstractRosterViewPanel_finishedSolving = "AbstractRosterViewPanel.finishedSolving";
-	
-	@TranslationKey(defaultValue="Name")
+
+    @TranslationKey(defaultValue="Solve")
+    final String AbstractRosterViewPanel_solve = "AbstractRosterViewPanel.solve";
+
+    @TranslationKey(defaultValue="Solving for another")
+    final String AbstractRosterViewPanel_solvingFor = "AbstractRosterViewPanel.solvingFor";
+
+    @TranslationKey(defaultValue="AbstractRosterViewPanel.startSolving")
+    final String AbstractRosterViewPanel_startSolving = "Click the <i>Solve</i> button to start solving.";
+
+    @TranslationKey(defaultValue="AbstractRosterViewPanel.terminateEarly")
+    final String AbstractRosterViewPanel_terminateEarly = "Terminate";
+    
+    
+
+    @TranslationKey(defaultValue="Employee name")
+    final String EmployeeListPanel_employeeName = "EmployeeListPanel.employeeName";
+    
+    
+
+    @TranslationKey(defaultValue="Actions")
+    final String General_actions = "General.actions";
+
+    @TranslationKey(defaultValue="Add")
+    final String General_add = "General.add";
+
+    @TranslationKey(defaultValue="Delete")
+    final String General_delete = "General.delete";
+
+    @TranslationKey(defaultValue="Employee")
+    final String General_employee = "General.employee";
+
+    @TranslationKey(defaultValue="Name")
     final String General_name = "General.name";
-	
-	@TranslationKey(defaultValue="Refresh")
-	final String General_refresh = "General.refresh";
-	
-	@TranslationKey(defaultValue="Rest API")
+
+    @TranslationKey(defaultValue="Refresh")
+    final String General_refresh = "General.refresh";
+
+    @TranslationKey(defaultValue="Skills")
+    final String General_skills = "General.skills";    
+    
+    
+
+    @TranslationKey(defaultValue="Employee Roster")
+    final String MenuPanel_employeeRoster = "MenuPanel.employeeRoster";
+
+    @TranslationKey(defaultValue="Employees")
+    final String MenuPanel_employees = "MenuPanel.employees";
+
+    @TranslationKey(defaultValue="Rest API")
     final String MenuPanel_restAPI = "MenuPanel.restAPI";
 
-	@TranslationKey(defaultValue="Solving for another")
-    final String AbstractRosterViewPanel_solvingFor = "AbstractRosterViewPanel.solvingFor";
-	
-	@TranslationKey(defaultValue="Required skill")
-	final String SpotListPanel_requiredSkill = "SpotListPanel.requiredSkill";
-	
-
-	
-	@TranslationKey(defaultValue="Skill name")
-	final String SkillListPanel_skillName = "SkillListPanel.skillName";
-	
-	@TranslationKey(defaultValue="Skills")
-    final String General_skills = "General.skills";
-	
-	
-	
-	@TranslationKey(defaultValue="Solve")
-	final String AbstractRosterViewPanel_solve = "AbstractRosterViewPanel.solve";
-	
-	
-
-	@TranslationKey(defaultValue="Spot")
-    final String SpotRosterView_spot = "SpotRosterView.spot";
-	
-	@TranslationKey(defaultValue="Spot name")
-	final String SpotListPanel_spotName = "SpotListPanel.spotName";
-	
-	@TranslationKey(defaultValue="Spot Roster")
+    @TranslationKey(defaultValue="Spot Roster")
     final String MenuPanel_spotRoster = "MenuPanel.spotRoster";
-	
-	@TranslationKey(defaultValue="Spots")
+
+    @TranslationKey(defaultValue="Spots")
     final String MenuPanel_spots = "MenuPanel.spots";
-	
-	
-	
-	@TranslationKey(defaultValue="Tenant")
+
+    @TranslationKey(defaultValue="Tenant")
     final String MenuPanel_tenant = "MenuPanel.tenant";
-	
-	@TranslationKey(defaultValue="Toggle navigation")
-    final String MenuPanel_toggleNavigation = "MenuPanel.toggleNavigation";	
+
+    @TranslationKey(defaultValue="Toggle navigation")
+    final String MenuPanel_toggleNavigation = "MenuPanel.toggleNavigation";
+    
+    
+    
+    @TranslationKey(defaultValue="Skill name")
+    final String SkillListPanel_skillName = "SkillListPanel.skillName";
+    
+    
+    
+    @TranslationKey(defaultValue="Required skill")
+    final String SpotListPanel_requiredSkill = "SpotListPanel.requiredSkill";
+
+    @TranslationKey(defaultValue="Spot name")
+    final String SpotListPanel_spotName = "SpotListPanel.spotName";
+    
+    
+
+    @TranslationKey(defaultValue="Spot")
+    final String SpotRosterView_spot = "SpotRosterView.spot";	
 }
