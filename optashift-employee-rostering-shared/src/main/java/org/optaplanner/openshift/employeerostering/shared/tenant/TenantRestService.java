5186a067eecb0aec85d3eb4a57e7448ad7faf69c
Duncan Doyle <ddoyle@redhat.com>
2018-02-01 00:14:01 +0100

    Removed unused configurations in Swagger BeanConfig setup. Added Swagger API annotation to TenantRestService.

--- optashift-employee-rostering-shared/src/main/java/org/optaplanner/openshift/employeerostering/shared/tenant/TenantRestService.java
+++ optashift-employee-rostering-shared/src/main/java/org/optaplanner/openshift/employeerostering/shared/tenant/TenantRestService.java
@@ -1,8 +1,8 @@
 package org.optaplanner.openshift.employeerostering.shared.tenant;
 
 import java.util.List;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,8 +10,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.github.nmorel.gwtjackson.rest.processor.GenRestBuilder;
 import org.optaplanner.openshift.employeerostering.shared.common.AbstractPersistable;
+
+import com.github.nmorel.gwtjackson.rest.processor.GenRestBuilder;
+
+import io.swagger.annotations.Api;
 
 @Api(tags = { "Tenant" })
 @Path("/tenant")
