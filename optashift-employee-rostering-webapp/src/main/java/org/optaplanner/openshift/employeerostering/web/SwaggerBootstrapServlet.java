e1920a9e24c0d93dff7821868380dd4de97ad4c0
Duncan Doyle <ddoyle@redhat.com>
2018-02-01 00:14:01 +0100

    Removed unused configurations in Swagger BeanConfig setup. Added Swagger API annotation to TenantRestService.

--- optashift-employee-rostering-webapp/src/main/java/org/optaplanner/openshift/employeerostering/web/SwaggerBootstrapServlet.java
+++ optashift-employee-rostering-webapp/src/main/java/org/optaplanner/openshift/employeerostering/web/SwaggerBootstrapServlet.java
@@ -13,9 +13,9 @@ public class SwaggerBootstrapServlet extends HttpServlet {
 		super.init(config);
 
 		BeanConfig beanConfig = new BeanConfig();
-		beanConfig.setVersion("1.0.2");
-		beanConfig.setSchemes(new String[]{"http"});
-		beanConfig.setHost("localhost:8080");
+		//beanConfig.setVersion("1.0.2");
+		//beanConfig.setSchemes(new String[]{"http"});
+		//beanConfig.setHost("localhost:8080");
 		beanConfig.setBasePath("/rest");
 		beanConfig.setResourcePackage("org.optaplanner.openshift.employeerostering");
 		beanConfig.setScan(true);
