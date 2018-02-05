f0c0fcb44809a430a8263c0ae08a4699b7319c51
Tiago Bento <tiagobentofernandes@gmail.com>
2018-02-05 00:02:26 -0200

    Create data.js file

--- optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/beta/grid.js
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/beta/grid.js
@@ -4,24 +4,36 @@ function scale(coordinate) {
 
 function drawGrid($viewport) {
 
-    var $parent = $('<div />', {
+    var $gridContainer = $('<div />', {
         class: 'grid',
         width: $viewport.width(),
         height: $viewport.height()
     }).appendTo('.grid-container');
 
-    for (var i = 0; i < HANDLES.gridSize($viewport); i += HANDLES.pixelSize()) {
+    $gridContainer.on("click", function (e) {
+        if (e.altKey) {
+            var $lane = addLaneTo($viewport);
+            addSubLaneTo($lane);
+        }
+    });
+
+    for (var i = 0; i < HANDLES.size($viewport); i += HANDLES.pixelSize()) {
         var $gridDiv = $('<div />');
         HANDLES.decorateGridDiv($gridDiv);
-        $gridDiv.appendTo($parent);
+        $gridDiv.appendTo($gridContainer);
     }
+
 }
 
-function toggleDraggablity($blob, $lock) {
-    if ($lock.hasClass("locked")) {
+function setLocked($blob, $lock, locked) {
+    if (locked) {
+        $lock.removeClass("unlocked");
+        $lock.addClass("locked");
         $blob.draggable('disable');
         $blob.resizable('disable');
     } else {
+        $lock.removeClass("locked");
+        $lock.addClass("unlocked");
         $blob.resizable('enable');
         $blob.draggable('enable');
     }
@@ -43,17 +55,15 @@ function bindBlobEvents($blob) {
     var $actions = $('<div />', {class: 'actions'});
     $actions.appendTo($blob);
 
-    HANDLES.decorateResizeHandle($blob);
+    HANDLES.configureResizability($blob);
 
     var $lock = $('<div />', {class: 'lock unlocked hide'});
     $lock.appendTo($actions);
     $lock.click(function (e) {
         $lock.toggleClass('locked');
         $lock.toggleClass('unlocked');
-        toggleDraggablity($blob, $lock);
+        setLocked($blob, $lock, $lock.hasClass("locked"));
     });
-
-    toggleDraggablity($blob, $lock);
 
     var $close = $('<div />', {class: 'close hide'});
     $close.appendTo($actions);
@@ -76,42 +86,70 @@ function bindSubLaneEvents($subLane) {
             return false;
         }
 
+        var $target = $(e.target);
+
         // Delete sub-lane (SHIFT + MIDDLE-CLICK or SHIFT + ALT + CLICK)
         if (e.shiftKey && e.button === 1 || e.shiftKey && e.altKey) {
-            e.target.remove();
+            if (!$target.parent().hasChildren) {
+                $target.parent().remove();
+            } else {
+                $target.remove();
+            }
         }
 
         // Add sub-lane (SHIFT + CLICK)
         else if (e.shiftKey) {
-            addSubLaneTo($(e.target).parent());
+            addSubLaneTo($target.parent());
         }
 
         // Add blob (ALT + CLICK)
         else if (e.altKey) {
-            addBlobTo($(e.target), {x: e.offsetX, y: e.offsetY});
+            var offset = HANDLES.grid.orientation === "y" ? e.offsetY : e.offsetX;
+            addBlobTo($target, {
+                label: "New",
+                size: HANDLES.defaultBlobSize,
+                position: offset / HANDLES.pixelSize(),
+                locked: false
+            });
         }
     });
+}
+
+function addLaneTo($target) {
+    var $lane = $('<div />', {class: "lane"});
+    $lane.appendTo($target);
+    return $lane;
 }
 
 function addSubLaneTo($target) {
     var $subLane = $('<div />', {class: 'sub-lane'});
     $subLane.appendTo($target);
     bindSubLaneEvents($subLane);
+    return $subLane;
 }
 
-function addBlobTo($target, point) {
+function addBlobTo($target, blob) {
     var $blob = $('<div />', {class: 'blob'});
     $blob.appendTo($target);
-    HANDLES.decorateNewBlob($blob, point);
+    HANDLES.decorateNewBlob($blob, blob);
+    $blob.prepend(blob.label);
     bindBlobEvents($blob);
+    setLocked($blob, $blob.find(".lock"), blob.locked);
+    return $blob;
 }
 
-$(".sub-lane").each(function () {
-    bindSubLaneEvents($(this));
-});
+var $viewport = $('.viewport');
 
-$(".blob").each(function () {
-    bindBlobEvents($(this));
-});
+drawGrid($viewport);
 
-drawGrid($('.viewport'));
+$(data.lanes).each(function (i, lane) {
+    var $lane = addLaneTo($viewport);
+
+    $(lane.subLanes).each(function (j, subLane) {
+        var $subLane = addSubLaneTo($lane);
+
+        $(subLane.blobs).each(function (k, blob) {
+            addBlobTo($subLane, blob);
+        });
+    });
+});
