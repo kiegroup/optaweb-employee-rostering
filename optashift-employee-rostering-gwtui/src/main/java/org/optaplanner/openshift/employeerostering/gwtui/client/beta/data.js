bc29c9b82de992bff29fd404e05040de52a4d432
Tiago Bento <tiagobentofernandes@gmail.com>
2018-02-05 00:02:26 -0200

    Create data.js file

---
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/beta/data.js
@@ -0,0 +1,48 @@
+var data = {
+    lanes: [{
+        subLanes: [{
+            blobs: [{
+                label: "Morning",
+                position: 0,
+                size: 4,
+                locked: true
+            }, {
+                label: "Night",
+                position: 8,
+                size: 2,
+                locked: false
+            }, {
+                label: "Afternoon",
+                position: 4,
+                size: 4,
+                locked: true
+            }]
+        }, {
+            blobs: [{
+                label: "Lunch",
+                position: 3,
+                size: 2,
+                locked: false
+            }]
+        }]
+    }, {
+        subLanes: [{
+            blobs: [{
+                label: "Morning",
+                position: 0,
+                size: 4,
+                locked: true
+            }, {
+                label: "Night",
+                position: 8,
+                size: 2,
+                locked: false
+            }, {
+                label: "Afternoon",
+                position: 4,
+                size: 4,
+                locked: false
+            }]
+        }]
+    }]
+};
