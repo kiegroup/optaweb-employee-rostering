cbfa6c489ca4fbe02f69f0a980c9d63538f61f7a
Tiago Bento <tiagobentofernandes@gmail.com>
2018-02-20 18:16:03 -0300

    Add configuration template to the Rotations screen

---
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/pages/rotation/RotationsConfigurationView.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.openshift.employeerostering.gwtui.client.pages.rotation;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import elemental2.dom.HTMLInputElement;
+import elemental2.dom.HTMLSelectElement;
+import org.jboss.errai.ui.client.local.api.elemental2.IsElement;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.optaplanner.openshift.employeerostering.gwtui.client.tenant.TenantStore;
+import org.optaplanner.openshift.employeerostering.shared.tenant.TenantConfiguration;
+
+@Templated
+public class RotationsConfigurationView implements IsElement {
+
+    @Inject
+    @DataField("undesired-time-slot-weight")
+    private HTMLInputElement undesiredTimeSlotWeight;
+
+    @Inject
+    @DataField("desired-time-slot-weight")
+    private HTMLInputElement desiredTimeSlotWeight;
+
+    @Inject
+    @DataField("week-start")
+    private HTMLSelectElement weekStart;
+
+    @Inject
+    @DataField("period")
+    private HTMLSelectElement period;
+
+    @Inject
+    private TenantStore tenantStore;
+
+    @PostConstruct
+    public void init() {
+        final TenantConfiguration configuration = tenantStore.getCurrentTenant().getConfiguration();
+
+        undesiredTimeSlotWeight.value = configuration.getUndesiredTimeSlotWeight().toString();
+        desiredTimeSlotWeight.value = configuration.getDesiredTimeSlotWeight().toString();
+        weekStart.value = configuration.getWeekStart().toString();
+        period.value = configuration.getTemplateDuration().toString();
+    }
+}
