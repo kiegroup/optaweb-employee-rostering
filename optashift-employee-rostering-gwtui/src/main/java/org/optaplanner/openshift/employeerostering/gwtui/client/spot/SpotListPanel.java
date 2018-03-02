85e6c887bee124a04867d4c2eab491d7419103cf
Christopher Chianelli <chris.chianelli@gmail.com>
2018-03-02 13:34:42 -0500

    Refactored code

--- optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/spot/SpotListPanel.java
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/spot/SpotListPanel.java
@@ -2,70 +2,41 @@ package org.optaplanner.openshift.employeerostering.gwtui.client.spot;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.cellview.client.Column;
-import com.google.gwt.user.cellview.client.SimplePager;
-import com.google.gwt.user.cellview.client.TextColumn;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.view.client.ListDataProvider;
+import elemental2.dom.HTMLButtonElement;
+import elemental2.dom.MouseEvent;
 import elemental2.promise.Promise;
-import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.client.ui.Pagination;
-import org.gwtbootstrap3.client.ui.constants.ButtonSize;
-import org.gwtbootstrap3.client.ui.constants.ButtonType;
-import org.gwtbootstrap3.client.ui.constants.IconType;
-import org.gwtbootstrap3.client.ui.gwt.ButtonCell;
-import org.gwtbootstrap3.client.ui.gwt.CellTable;
-import org.gwtbootstrap3.extras.tagsinput.client.ui.base.SingleValueTagsInput;
-import org.gwtbootstrap3.extras.typeahead.client.base.CollectionDataset;
-import org.jboss.errai.databinding.client.api.DataBinder;
 import org.jboss.errai.databinding.client.components.ListComponent;
 import org.jboss.errai.databinding.client.components.ListContainer;
-import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.jboss.errai.ui.client.local.api.elemental2.IsElement;
-import org.jboss.errai.ui.client.local.spi.TranslationService;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
+import org.jboss.errai.ui.shared.api.annotations.ForEvent;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
-import org.optaplanner.openshift.employeerostering.gwtui.client.common.CommonUtils;
 import org.optaplanner.openshift.employeerostering.gwtui.client.common.DataInvalidation;
 import org.optaplanner.openshift.employeerostering.gwtui.client.common.FailureShownRestCallback;
 import org.optaplanner.openshift.employeerostering.gwtui.client.common.KiePager;
 import org.optaplanner.openshift.employeerostering.gwtui.client.common.KieSearchBar;
-import org.optaplanner.openshift.employeerostering.gwtui.client.employee.EmployeeSubform;
 import org.optaplanner.openshift.employeerostering.gwtui.client.pages.Page;
 import org.optaplanner.openshift.employeerostering.gwtui.client.tenant.TenantStore;
 import org.optaplanner.openshift.employeerostering.gwtui.client.util.PromiseUtils;
-import org.optaplanner.openshift.employeerostering.shared.employee.Employee;
-import org.optaplanner.openshift.employeerostering.shared.skill.Skill;
-import org.optaplanner.openshift.employeerostering.shared.skill.SkillRestServiceBuilder;
 import org.optaplanner.openshift.employeerostering.shared.spot.Spot;
 import org.optaplanner.openshift.employeerostering.shared.spot.SpotRestServiceBuilder;
 
-import static org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n.OptaShiftUIConstants.General_actions;
-import static org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n.OptaShiftUIConstants.General_delete;
-import static org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n.OptaShiftUIConstants.General_edit;
-import static org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n.OptaShiftUIConstants.General_name;
-import static org.optaplanner.openshift.employeerostering.gwtui.client.resources.i18n.OptaShiftUIConstants.General_skills;
-
 @Templated
 public class SpotListPanel implements IsElement,
-        Page {
+                           Page {
 
     @Inject
     @DataField
-    private Button refreshButton;
+    private HTMLButtonElement refreshButton;
     @Inject
     @DataField
-    private Button addButton;
+    private HTMLButtonElement addButton;
 
     @Inject
     @DataField
@@ -78,17 +49,13 @@ public class SpotListPanel implements IsElement,
     @Inject
     private TenantStore tenantStore;
 
-    @Inject
-    private TranslationService CONSTANTS;
-
     // TODO use DataGrid instead
     @Inject
     @DataField
     @ListContainer("table")
     private ListComponent<Spot, SpotSubform> table;
 
-    public SpotListPanel() {
-    }
+    public SpotListPanel() {}
 
     @PostConstruct
     protected void initWidget() {
@@ -109,12 +76,21 @@ public class SpotListPanel implements IsElement,
     }
 
     @EventHandler("refreshButton")
-    public void refresh(ClickEvent e) {
+    public void refresh(final @ForEvent("click") MouseEvent e) {
         refresh();
     }
 
     public Promise<Void> refresh() {
-        return refreshTable();
+        if (tenantStore.getCurrentTenantId() == null) {
+            return PromiseUtils.resolve();
+        }
+        return new Promise<>((res, rej) -> {
+            SpotRestServiceBuilder.getSpotList(tenantStore.getCurrentTenantId(), FailureShownRestCallback
+                                                                                                         .onSuccess(newSpotList -> {
+                                                                                                             pager.setData(newSpotList);
+                                                                                                             res.onInvoke(PromiseUtils.resolve());
+                                                                                                         }));
+        });
     }
 
     private void initTable() {
@@ -122,21 +98,8 @@ public class SpotListPanel implements IsElement,
         pager.setPresenter(table);
     }
 
-    private Promise<Void> refreshTable() {
-        if (tenantStore.getCurrentTenantId() == null) {
-            return PromiseUtils.resolve();
-        }
-        return new Promise<>((res, rej) -> {
-            SpotRestServiceBuilder.getSpotList(tenantStore.getCurrentTenantId(), FailureShownRestCallback
-                    .onSuccess(newSpotList -> {
-                        pager.setData(newSpotList);
-                        res.onInvoke(PromiseUtils.resolve());
-                    }));
-        });
-    }
-
     @EventHandler("addButton")
-    public void add(ClickEvent e) {
+    public void add(final @ForEvent("click") MouseEvent e) {
         SpotSubform.createNewRow(new Spot(tenantStore.getCurrentTenantId(), "", new HashSet<>()), table, pager);
     }
 }
