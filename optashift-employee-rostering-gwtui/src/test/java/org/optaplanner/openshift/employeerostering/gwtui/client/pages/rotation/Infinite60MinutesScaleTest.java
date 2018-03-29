abb196e371289d86dd8df9186a6e77a97208cd78
Christopher Chianelli <chris.chianelli@gmail.com>
2018-03-29 13:12:43 -0400

    Fixed the test

--- optashift-employee-rostering-gwtui/src/test/java/org/optaplanner/openshift/employeerostering/gwtui/client/pages/rotation/Infinite60MinutesScaleTest.java
+++ optashift-employee-rostering-gwtui/src/test/java/org/optaplanner/openshift/employeerostering/gwtui/client/pages/rotation/Infinite60MinutesScaleTest.java
@@ -1,33 +1,42 @@
 package org.optaplanner.openshift.employeerostering.gwtui.client.pages.rotation;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class Infinite60MinutesScaleTest {
 
     @Test
     public void test() {
 
-        final Long start = 0L;
-        final Long end = 10080L;
+        final OffsetDateTime start = OffsetDateTime.of(2018, 2, 7, 0, 0, 0, 0, ZoneOffset.UTC);
+        final OffsetDateTime end = start.plusDays(7);
+        final Long startInGridPixels = 0L;
+        final Long endInGridPixels = 24L * 7L;
 
-        final Infinite60MinutesScale scale = new Infinite60MinutesScale(end);
+        final Infinite60MinutesScale scale = new Infinite60MinutesScale(start, end);
 
         // Boundaries
-        Assert.assertEquals((Long) 0L, scale.toGridPixels(start));
-        Assert.assertEquals((Long) 168L, scale.toGridPixels(end));
-        Assert.assertEquals(start, scale.toScaleUnits(0L));
-        Assert.assertEquals(end, scale.toScaleUnits(168L));
+        Assert.assertEquals((Long) startInGridPixels, scale.toGridPixels(start));
+        Assert.assertEquals((Long) endInGridPixels, scale.toGridPixels(end));
+        Assert.assertEquals(start, scale.toScaleUnits(startInGridPixels));
+        Assert.assertEquals(end, scale.toScaleUnits(endInGridPixels));
 
         // Over-boundaries
-        Assert.assertEquals((Long) (-1L), scale.toGridPixels(start - 60L));
-        Assert.assertEquals((Long) 192L, scale.toGridPixels(end + 1440L));
-        Assert.assertEquals((Long) (-60L), scale.toScaleUnits(-1L));
-        Assert.assertEquals((Long) 11520L, scale.toScaleUnits(192L));
+        Assert.assertEquals((Long) (-1L), scale.toGridPixels(start.minus(60, MINUTES)));
+        Assert.assertEquals((Long) (endInGridPixels + 1L), scale.toGridPixels(end.plus(60, MINUTES)));
+        Assert.assertEquals(start.minusMinutes(60L), scale.toScaleUnits(startInGridPixels - 1L));
+        Assert.assertEquals(end.plusMinutes(60L), scale.toScaleUnits(endInGridPixels + 1));
 
         // 3 Days and 4 Hours after
-        final Long d = start + 4560L;
-        Assert.assertEquals((Long) 76L, scale.toGridPixels(d));
-        Assert.assertEquals(d, scale.toScaleUnits(76L));
+        final OffsetDateTime d = start.plus(3, DAYS).plus(4, HOURS);
+        Assert.assertEquals((Long) (24L * 3 + 4L), scale.toGridPixels(d));
+        Assert.assertEquals(d, scale.toScaleUnits(24L * 3 + 4L));
     }
 }
