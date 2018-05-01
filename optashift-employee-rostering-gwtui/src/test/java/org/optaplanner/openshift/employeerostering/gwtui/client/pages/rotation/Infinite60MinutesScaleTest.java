5a22711a350292a6891440cc54cff6f978986266
Christopher Chianelli <chris.chianelli@gmail.com>
2018-05-01 09:32:42 -0400

    Fixed tests

--- optashift-employee-rostering-gwtui/src/test/java/org/optaplanner/openshift/employeerostering/gwtui/client/pages/rotation/Infinite60MinutesScaleTest.java
+++ optashift-employee-rostering-gwtui/src/test/java/org/optaplanner/openshift/employeerostering/gwtui/client/pages/rotation/Infinite60MinutesScaleTest.java
@@ -11,31 +11,33 @@ import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class Infinite60MinutesScaleTest {
 
+    private static final Double ACCEPTED_ERROR = Double.MIN_VALUE;
+
     @Test
     public void test() {
 
         final LocalDateTime start = LocalDateTime.of(2018, 2, 7, 0, 0, 0, 0);
         final LocalDateTime end = start.plusDays(7);
-        final Long startInGridPixels = 0L;
-        final Long endInGridPixels = 24L * 7L;
+        final Double startInGridPixels = 0d;
+        final Double endInGridPixels = 24d * 7d;
 
         final Infinite60MinutesScale scale = new Infinite60MinutesScale(start, end);
 
         // Boundaries
-        Assert.assertEquals((Long) startInGridPixels, scale.toGridPixels(start));
-        Assert.assertEquals((Long) endInGridPixels, scale.toGridPixels(end));
+        Assert.assertEquals((Double) startInGridPixels, scale.toGridPixels(start), ACCEPTED_ERROR);
+        Assert.assertEquals((Double) endInGridPixels, scale.toGridPixels(end), ACCEPTED_ERROR);
         Assert.assertEquals(start, scale.toScaleUnits(startInGridPixels));
         Assert.assertEquals(end, scale.toScaleUnits(endInGridPixels));
 
         // Over-boundaries
-        Assert.assertEquals((Long) (-1L), scale.toGridPixels(start.minus(60, MINUTES)));
-        Assert.assertEquals((Long) (endInGridPixels + 1L), scale.toGridPixels(end.plus(60, MINUTES)));
+        Assert.assertEquals((Double) (-1d), scale.toGridPixels(start.minus(60, MINUTES)), ACCEPTED_ERROR);
+        Assert.assertEquals((Double) (endInGridPixels + 1d), scale.toGridPixels(end.plus(60, MINUTES)), ACCEPTED_ERROR);
         Assert.assertEquals(start.minusMinutes(60L), scale.toScaleUnits(startInGridPixels - 1L));
         Assert.assertEquals(end.plusMinutes(60L), scale.toScaleUnits(endInGridPixels + 1));
 
         // 3 Days and 4 Hours after
         final LocalDateTime d = start.plus(3, DAYS).plus(4, HOURS);
-        Assert.assertEquals((Long) (24L * 3 + 4L), scale.toGridPixels(d));
-        Assert.assertEquals(d, scale.toScaleUnits(24L * 3 + 4L));
+        Assert.assertEquals((Double) (24d * 3 + 4d), scale.toGridPixels(d), ACCEPTED_ERROR);
+        Assert.assertEquals(d, scale.toScaleUnits(24d * 3 + 4d));
     }
 }
