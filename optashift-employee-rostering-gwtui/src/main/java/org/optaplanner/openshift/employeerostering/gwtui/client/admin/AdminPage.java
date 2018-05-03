676c5f04103639774ee740c5a5cc7e6509abff0d
Christopher Chianelli <chris.chianelli@gmail.com>
2018-05-03 12:13:23 -0400

    Made clicking the reset button shown a loading screen and used a notification system instead of using an ErrorPopup

--- optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/admin/AdminPage.java
+++ optashift-employee-rostering-gwtui/src/main/java/org/optaplanner/openshift/employeerostering/gwtui/client/admin/AdminPage.java
@@ -8,9 +8,10 @@ import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.ForEvent;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.optaplanner.openshift.employeerostering.gwtui.client.app.spinner.LoadingSpinner;
 import org.optaplanner.openshift.employeerostering.gwtui.client.common.FailureShownRestCallback;
+import org.optaplanner.openshift.employeerostering.gwtui.client.common.NotificationSystem;
 import org.optaplanner.openshift.employeerostering.gwtui.client.pages.Page;
-import org.optaplanner.openshift.employeerostering.gwtui.client.popups.ErrorPopup;
 import org.optaplanner.openshift.employeerostering.shared.admin.AdminRestServiceBuilder;
 
 @Templated
@@ -19,11 +20,19 @@ public class AdminPage implements Page {
     @Inject
     @DataField("reset-application-button")
     HTMLButtonElement resetApplicationButton;
+    
+    @Inject
+    private LoadingSpinner loadingSpinner;
+    
+    @Inject
+    private NotificationSystem notificationSystem;
 
     @EventHandler("reset-application-button")
     private void resetApplication(@ForEvent("click") MouseEvent e) {
+        loadingSpinner.showFor("reset-application");
         AdminRestServiceBuilder.resetApplication(null, FailureShownRestCallback.onSuccess((success) -> {
-            ErrorPopup.show("Application was successfully reset");
+            loadingSpinner.hideFor("reset-application");
+            notificationSystem.notify("Application was reset successfully", "Application was reset successfully, please refresh the page.");
         }));
     }
 }
