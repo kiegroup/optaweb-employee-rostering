1c8b071fa7cfe42105ceaae277dc18e65340191d
Radovan Synek <rsynek@redhat.com>
2019-01-03 14:33:18 +0100

    Enable PR analysis in SonarCloud

---
+++ travis-sonarcloud.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]
+then
+  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering -Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.github.repository=$TRAVIS_PULL_REQUEST_SLUG
+else
+  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering
+fi
