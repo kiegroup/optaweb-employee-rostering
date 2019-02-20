4c8d826775e433cccdcdb856cadca9dd975bf83c
Jiří Locker <jiri.locker@gmail.com>
2019-02-20 14:46:49 +0100

    PLANNER-936: Fix other code style issues

--- employee-rostering-webapp/src/main/java/org/optaweb/employeerostering/web/SwaggerBootstrapServlet.java
+++ employee-rostering-webapp/src/main/java/org/optaweb/employeerostering/web/SwaggerBootstrapServlet.java
@@ -24,17 +24,16 @@ import io.swagger.jaxrs.config.BeanConfig;
 
 public class SwaggerBootstrapServlet extends HttpServlet {
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
 
-		BeanConfig beanConfig = new BeanConfig();
-		//beanConfig.setVersion("1.0.2");
-		//beanConfig.setSchemes(new String[]{"http"});
-		//beanConfig.setHost("localhost:8080");
-		beanConfig.setBasePath("/rest");
-		beanConfig.setResourcePackage("org.optaweb.employeerostering");
-		beanConfig.setScan(true);
-	}
-
+        BeanConfig beanConfig = new BeanConfig();
+        //beanConfig.setVersion("1.0.2");
+        //beanConfig.setSchemes(new String[]{"http"});
+        //beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/rest");
+        beanConfig.setResourcePackage("org.optaweb.employeerostering");
+        beanConfig.setScan(true);
+    }
 }
