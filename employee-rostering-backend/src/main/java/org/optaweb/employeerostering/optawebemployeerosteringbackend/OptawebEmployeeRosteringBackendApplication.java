a88c9a489ae453c6a95c2d3e52d9243537f70c52
Julian Cui <jucui@redhat.com>
2019-05-31 16:33:34 -0400

    [PLANNER-1521] Initialize Spring Boot application for backend
    Added autogenerated Spring Application file and test file from Spring Initializr

---
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/optawebemployeerosteringbackend/OptawebEmployeeRosteringBackendApplication.java
@@ -0,0 +1,13 @@
+package org.optaweb.employeerostering.optawebemployeerosteringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptawebEmployeeRosteringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptawebEmployeeRosteringBackendApplication.class, args);
+	}
+
+}
---
+++ employee-rostering-backend/src/test/java/org/optaweb/employeerostering/optawebemployeerosteringbackend/OptawebEmployeeRosteringBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package org.optaweb.employeerostering.optawebemployeerosteringbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OptawebEmployeeRosteringBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
