ed4973166d047bd07a0ccb8e1ec1b2104cb50476
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-05 11:40:56 -0400

    Add Tenant Selector

--- employee-rostering-frontend/src/ui/header/Toolbar.tsx
+++ employee-rostering-frontend/src/ui/header/Toolbar.tsx
@@ -26,29 +26,111 @@ import {
 } from '@patternfly/react-core';
 import { BellIcon, CogIcon } from '@patternfly/react-icons';
 import * as React from 'react';
+import Tenant from '../../domain/Tenant';
+import TenantRestServiceClient from '../../services/TenantRestServiceClient';
+import {isUndefined} from "util";
 
-const ToolbarComponent: React.FC = () => {
-  const [openTenants, setOpenTenants] = React.useState(false);
-  return (
-    <Toolbar
-      style={{ display: 'none' }} // TODO remove this when we start using tenants
-    >
+interface ToolbarProps {}
+interface ToolbarState {
+  currentTenant?: Tenant,
+  tenantList : Tenant[],
+  isTenantSelectOpen : boolean;
+}
+
+class ToolbarComponent extends React.Component<ToolbarProps, ToolbarState> {
+  tenantRestService : TenantRestServiceClient = new TenantRestServiceClient();
+  toolbarState : ToolbarState;
+
+  constructor(props : ToolbarProps) {
+    super(props);
+    this.toolbarState = {tenantList: [], isTenantSelectOpen : false};;
+    this.updateTenantList();
+  }
+
+  updateTenantList() {
+    this.tenantRestService.getTenantList().then(tenantList => {
+      if (tenantList.length > 0) {
+        if (isUndefined(this.toolbarState.currentTenant) || !tenantList.includes(this.toolbarState.currentTenant)) {
+          this.toolbarState = {
+            currentTenant: tenantList[0],
+            tenantList: tenantList,
+            isTenantSelectOpen : this.toolbarState.isTenantSelectOpen
+          };
+        }
+        else {
+          this.toolbarState = {
+            currentTenant: this.toolbarState.currentTenant,
+            tenantList: tenantList,
+            isTenantSelectOpen : this.toolbarState.isTenantSelectOpen
+          };
+        }
+      }
+      else {
+        this.toolbarState = {tenantList: [], isTenantSelectOpen: this.toolbarState.isTenantSelectOpen};
+      }
+      this.refresh();
+    });
+  }
+
+  setCurrentTenant(newTenantId : number) {
+    this.toolbarState = {
+      currentTenant: this.toolbarState.tenantList[newTenantId],
+      tenantList: this.toolbarState.tenantList,
+      isTenantSelectOpen : false
+    };
+    this.refresh();
+  }
+
+  setIsTenantSelectOpen(isOpen : boolean) {
+    this.toolbarState = {currentTenant: this.toolbarState.currentTenant, tenantList: this.toolbarState.tenantList, isTenantSelectOpen: isOpen};
+    this.refresh();
+  }
+
+  refresh() {
+    this.setState(this.toolbarState);
+  }
+
+  render() {
+    if (isUndefined(this.toolbarState.currentTenant)) {
+      return  <Toolbar><ToolbarGroup/> <ToolbarGroup>
+      <ToolbarItem>
+        <Button
+          id="horizontal-example-uid-01"
+          aria-label="Notifications actions"
+          variant={ButtonVariant.plain}
+        >
+          <BellIcon />
+        </Button>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Button
+          id="horizontal-example-uid-02"
+          aria-label="Settings actions"
+          variant={ButtonVariant.plain}
+        >
+          <CogIcon />
+        </Button>
+      </ToolbarItem>
+    </ToolbarGroup></Toolbar>
+    }
+    else {
+      let {currentTenant, tenantList, isTenantSelectOpen} = this.toolbarState;
+      return <Toolbar>
       <ToolbarGroup>
         <ToolbarItem>
           <Dropdown
             isPlain={true}
             position="right"
             // tslint:disable-next-line:no-console
-            onSelect={event => console.log(event)}
-            isOpen={openTenants}
+            onSelect={event => this.setCurrentTenant(parseInt(event.currentTarget.id))}
+            isOpen={isTenantSelectOpen}
             toggle={
-              <DropdownToggle onToggle={() => setOpenTenants(!openTenants)}>
-                Tenant Name
+              <DropdownToggle onToggle={() => this.setIsTenantSelectOpen(!isTenantSelectOpen)}>
+                {currentTenant.name}
               </DropdownToggle>}
-            dropdownItems={[
-              <DropdownItem key={0}>ACMEE Corp</DropdownItem>,
-              <DropdownItem key={1}>Wayne Ent.</DropdownItem>,
-            ]}
+            dropdownItems={tenantList.map((tenant, index) => {
+              return <DropdownItem id={index.toString()} key={index}>{tenant.name}</DropdownItem>;
+            })}
           />
         </ToolbarItem>
       </ToolbarGroup>
@@ -73,7 +155,8 @@ const ToolbarComponent: React.FC = () => {
         </ToolbarItem>
       </ToolbarGroup>
     </Toolbar>
-  );
-};
+    }
+  }
+}
 
 export default ToolbarComponent;
