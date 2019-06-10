34ae5beddadfb2ca0dc613c1cdaf13d9c85ff934
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-10 16:00:51 -0400

    Add snapshot tests for EditableComponent

---
+++ employee-rostering-frontend/src/ui/components/EditableComponent.test.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import * as React from 'react';
+import { EditableComponent, EditableComponentProps } from './EditableComponent';
+
+describe('EditableComponent component', () => {
+  it('should render viewer when not editing', () => {
+    const component = shallow(<EditableComponent {...editableComponent} />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render editor when editing', () => {
+    const component = shallow(<EditableComponent {...editableComponent} />);
+    (component.instance() as EditableComponent).startEditing();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render viewer when stop editing', () => {
+    const component = shallow(<EditableComponent {...editableComponent} />);
+    (component.instance() as EditableComponent).startEditing();
+    (component.instance() as EditableComponent).stopEditing();
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
+
+const editableComponent: EditableComponentProps = {
+  viewer: <span>Viewer</span>,
+  editor: <div>Editor</div>
+};
