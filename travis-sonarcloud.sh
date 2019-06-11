eee39d796b94e80462ea8f4a117528a720161346
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-11 17:07:39 -0400

    Fixed travis-sonarcloud
    TODO: Find out why jacoco plugin resolution suddenly stop working
    on this branch

--- travis-sonarcloud.sh
+++ travis-sonarcloud.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+JACOCO=org.jacoco:jacoco-maven-plugin:0.7.5.201505241946
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]
 then
-  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering -Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.github.repository=$TRAVIS_PULL_REQUEST_SLUG
+  mvn --quiet $JACOCO:report $JACOCO:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering -Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.github.repository=$TRAVIS_PULL_REQUEST_SLUG
 else
-  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering
+  mvn --quiet $JACOCO:report $JACOCO:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering
 fi
