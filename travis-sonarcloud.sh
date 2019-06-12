71bd98a94436b2f3685c7e128f33246733e0feca
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-12 08:27:06 -0400

    Modify .travis.yml to run code coverage on frontend

--- travis-sonarcloud.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-JACOCO=org.jacoco:jacoco-maven-plugin:0.7.5.201505241946
-if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]
-then
-  mvn --quiet $JACOCO:report $JACOCO:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering -Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.github.repository=$TRAVIS_PULL_REQUEST_SLUG
-else
-  mvn --quiet $JACOCO:report $JACOCO:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=kiegroup -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=optaweb-employee-rostering
-fi
