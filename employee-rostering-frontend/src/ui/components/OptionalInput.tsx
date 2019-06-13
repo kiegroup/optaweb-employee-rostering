e432c72ccc1a609e2b6403d46383ed7b5538c999
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-13 14:18:16 -0400

    Add Contracts Page to React frontend

---
+++ employee-rostering-frontend/src/ui/components/OptionalInput.tsx
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import { Switch, TextInput } from '@patternfly/react-core';
+
+interface OptionalInputProps<T extends Object> {
+    defaultValue: T|null;
+    isValid: (value: string) => boolean;
+    valueMapper: (value: string) => T;
+    onChange: (value: T|null) => void;
+}
+
+interface OptionalInputState<T extends Object> {
+    inputValue: T|null;
+    currentValue: T|null;
+    isChecked: boolean;
+}
+
+export default class OptionalInput<T  extends Object> extends React.Component<OptionalInputProps<T>,OptionalInputState<T>> {
+  constructor(props:OptionalInputProps<T>) {
+    super(props);
+    this.state = {
+        inputValue: props.defaultValue,
+        currentValue: props.defaultValue,
+        isChecked: props.defaultValue !== null
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(isChecked: boolean) {
+      if (isChecked) {
+          this.setState(prevState => ({currentValue: prevState.inputValue, isChecked: true}));
+          this.props.onChange(this.state.inputValue);
+      }
+      else {
+          this.setState({currentValue: null, isChecked: false});
+          this.props.onChange(null);
+      }
+  }
+
+  render() {
+    const { isChecked } = this.state;
+    return (
+      <span>
+        <TextInput isDisabled={!isChecked}
+          defaultValue={(this.props.defaultValue !== null)? this.props.defaultValue.toString() : ''}
+          onChange={(value) => {
+            if (this.props.isValid(value)) {
+                const mappedValue = this.props.valueMapper(value);
+                this.setState({inputValue: mappedValue});
+                if (this.state.isChecked) {
+                    this.setState({currentValue: mappedValue});
+                    this.props.onChange(mappedValue);
+                }
+            }
+        }}/>
+        <Switch aria-label="Enabled" isChecked={isChecked} onChange={this.handleToggle} />
+      </span>
+    );
+  }
+}
