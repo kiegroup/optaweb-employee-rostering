f2012caaed8ad289c8697ffe403cb010a4dd3a81
Julian Cui <jucui@redhat.com>
2019-06-18 16:59:47 -0400

    Refactor SkillController using constructor dependency injection

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/controller/SkillController.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/controller/SkillController.java
@@ -20,9 +20,9 @@ import java.util.List;
 
 import org.optaweb.employeerostering.domain.Skill;
 import org.optaweb.employeerostering.service.SkillService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,8 +34,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SkillController {
 
-    @Autowired
-    private SkillService skillService;
+    private final SkillService skillService;
+
+    public SkillController(SkillService skillService) {
+        this.skillService = skillService;
+        Assert.notNull(skillService, "skillService must not be null.");
+    }
 
     @GetMapping("/tenant/{tenantId}/skill")
     public ResponseEntity<List> getSkillList(@PathVariable Integer tenantId) {
