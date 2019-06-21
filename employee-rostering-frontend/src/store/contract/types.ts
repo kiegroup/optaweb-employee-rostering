ef0d799cd9b34b4a223275c60221eecce6429aa0
Christopher Chianelli <chris.chianelli@gmail.com>
2019-06-21 09:30:17 -0400

    PLANNER-1539: begin refactor

--- employee-rostering-frontend/src/store/contract/types.ts
+++ employee-rostering-frontend/src/store/contract/types.ts
@@ -16,12 +16,18 @@
 
 import { Action } from 'redux';
 import Contract from 'domain/Contract';
+import DomainObjectView from 'domain/DomainObjectView';
 
 export enum ActionType {
   ADD_CONTRACT = 'ADD_CONTRACT',
   REMOVE_CONTRACT = 'REMOVE_CONTRACT',
   UPDATE_CONTRACT = 'UPDATE_CONTRACT',
-  REFRESH_CONTRACT_LIST = 'REFRESH_CONTRACT_LIST'
+  REFRESH_CONTRACT_LIST = 'REFRESH_CONTRACT_LIST',
+  SET_CONTRACT_LIST_LOADING = 'SET_CONTRACT_LIST_LOADING'
+}
+
+export interface SetContractListLoadingAction extends Action<ActionType.SET_CONTRACT_LIST_LOADING> {
+  readonly isLoading: boolean;
 }
 
 export interface AddContractAction extends Action<ActionType.ADD_CONTRACT> {
@@ -40,8 +46,10 @@ export interface RefreshContractListAction extends Action<ActionType.REFRESH_CON
   readonly contractList: Contract[];
 }
 
-export type ContractAction = AddContractAction | RemoveContractAction | UpdateContractAction | RefreshContractListAction;
+export type ContractAction = SetContractListLoadingAction | AddContractAction | RemoveContractAction |
+  UpdateContractAction | RefreshContractListAction;
 
 export interface ContractList {
-  readonly contractList: Contract[];
+  readonly isLoading: boolean;
+  readonly contractMapById: Map<number, DomainObjectView<Contract>>;
 }
