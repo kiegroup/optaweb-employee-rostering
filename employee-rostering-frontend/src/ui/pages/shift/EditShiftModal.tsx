73a4b5f25e529fc417279e37b037fc3bd39d2b19
Christopher Chianelli <chris.chianelli@gmail.com>
2019-07-08 15:00:49 -0400

    Fix loading shifts; add refreshing, deleting and creating shifts

--- employee-rostering-frontend/src/ui/pages/shift/EditShiftModal.tsx
+++ employee-rostering-frontend/src/ui/pages/shift/EditShiftModal.tsx
@@ -16,7 +16,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Button, Switch, InputGroup, Label, Form, Modal } from '@patternfly/react-core';
+import { Button, Switch, InputGroup, Label, Form, Modal, ButtonVariant } from '@patternfly/react-core';
 import DatePicker from 'react-datepicker';
 
 import Shift from 'domain/Shift';
@@ -30,11 +30,13 @@ import "react-datepicker/dist/react-datepicker.css";
 import TypeaheadSelectInput from 'ui/components/TypeaheadSelectInput';
 
 interface Props {
+  tenantId: number;
   shift?: Shift;
   isOpen: boolean;
   employeeList: Employee[];
   spotList: Spot[];
   onSave: (shift: Shift) => void;
+  onDelete: (shift: Shift) => void;
   onClose: () => void;
 }
 
@@ -42,9 +44,11 @@ const mapStateToProps = (state: AppState, ownProps: {
   shift?: Shift;
   isOpen: boolean;
   onSave: (shift: Shift) => void;
+  onDelete: (shift: Shift) => void;
   onClose: () => void;
 }): Props => ({
   ...ownProps,
+  tenantId: state.tenantData.currentTenantId,
   employeeList: employeeSelectors.getEmployeeList(state),
   spotList: spotSelectors.getSpotList(state)
 }); 
@@ -64,9 +68,14 @@ class EditShiftModal extends React.Component<Props, State> {
   }
 
   componentDidUpdate(prevProps: Props, prevState: State) {
-    if (this.props.shift === undefined && prevState.editedValue.id !== undefined) {
+    if (this.props.shift === undefined && prevState.editedValue.pinnedByUser === undefined) {
       // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ editedValue: {} });
+      this.setState({ editedValue: {
+        tenantId: this.props.tenantId,
+        employee: null,
+        rotationEmployee: null,
+        pinnedByUser: false
+      } });
     }
     else if (this.props.shift !== undefined &&
       (this.props.shift.id !== prevState.editedValue.id || this.props.shift.version !== prevState.editedValue.version)) {
@@ -88,14 +97,17 @@ class EditShiftModal extends React.Component<Props, State> {
     const dateFormat = "MMMM dd, hh:mm a";
     return (
       <Modal
-        title="Edit Shift"
+        title={this.props.shift? "Edit Shift" : "Create Shift"}
         onClose={this.props.onClose}
         isOpen={this.props.isOpen}
         actions={
           [
-            <Button key={0} onClick={this.props.onClose}>Close</Button>,
-            <Button key={1} onClick={this.onSave}>Save</Button>
-          ]
+            <Button variant={ButtonVariant.tertiary} key={0} onClick={this.props.onClose}>Close</Button>
+          ].concat(this.props.shift? [
+            <Button variant={ButtonVariant.danger} key={0} onClick={() => this.props.onDelete(this.props.shift as Shift)}>Delete</Button>
+          ] : []).concat([
+            <Button key={2} onClick={this.onSave}>Save</Button>
+          ])
         }
         isSmall
       >
