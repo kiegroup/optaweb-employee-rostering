d9dc81a146b2ef4124f7065e48663db034f0dd46
Julian Cui <jucui@redhat.com>
2019-07-12 18:19:26 -0400

    Add TenantMockController to test frontend

---
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/controller/TenantMockController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.employeerostering.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.optaweb.employeerostering.domain.tenant.Tenant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/tenant")
+public class TenantMockController {
+
+    final Tenant TENANT = createTenant();
+
+    @GetMapping
+    public ResponseEntity<List> getTenantList() {
+        return new ResponseEntity<>(Arrays.asList(TENANT), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tenant> getTenant(@PathVariable Integer id) {
+        return new ResponseEntity<>(TENANT, HttpStatus.OK);
+    }
+
+    private Tenant createTenant(){
+        Tenant tenant = new Tenant("mockTenant");
+        tenant.setId(1);
+        tenant.setVersion(0L);
+        return tenant;
+    }
+}
