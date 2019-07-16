2ad3658ba0b9266fd6530728eb4963c5bdeefbda
Julian Cui <jucui@redhat.com>
2019-07-16 12:05:25 -0400

    Fix Skill field and formatting

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/SkillRepository.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/SkillRepository.java
@@ -23,6 +23,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SkillRepository extends JpaRepository <Skill, Long> {
+public interface SkillRepository extends JpaRepository<Skill, Long> {
     List<Skill> findAllByTenantId(Integer tenantId);
 }
