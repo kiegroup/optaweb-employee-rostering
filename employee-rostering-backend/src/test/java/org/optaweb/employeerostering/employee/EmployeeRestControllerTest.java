83263700d6d8e626d030e585486cb5b2246178e0
Julian Cui <jucui@redhat.com>
2019-07-19 16:10:39 -0400

    Refactor employee controller and service tests

---
+++ employee-rostering-backend/src/test/java/org/optaweb/employeerostering/employee/EmployeeRestControllerTest.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.employeerostering.employee;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.optaweb.employeerostering.domain.contract.Contract;
+import org.optaweb.employeerostering.domain.employee.Employee;
+import org.optaweb.employeerostering.domain.skill.Skill;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class EmployeeRestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private String employeePathURI = "http://localhost:8080/rest/tenant/{tenantId}/employee/";
+    private String contractPathURI = "http://localhost:8080/rest/tenant/{tenantId}/contract/";
+    private String skillPathURI = "http://localhost:8080/rest/tenant/{tenantId}/skill/";
+
+    private ResponseEntity<List<Employee>> getEmployees(Integer tenantId) {
+        return restTemplate.exchange(employeePathURI, HttpMethod.GET, null, new ParameterizedTypeReference
+                <List<Employee>>() {}, tenantId);
+    }
+
+    private ResponseEntity<Employee> getEmployee(Integer tenantId, Long id) {
+        return restTemplate.getForEntity(employeePathURI + id, Employee.class, tenantId);
+    }
+
+    private void deleteEmployee(Integer tenantId, Long id) {
+        restTemplate.delete(employeePathURI + id, tenantId);
+    }
+
+    private ResponseEntity<Employee> addEmployee(Integer tenantId, Employee employee) {
+        return restTemplate.postForEntity(employeePathURI + "add", employee, Employee.class, tenantId);
+    }
+
+    private ResponseEntity<Employee> updateEmployee(Integer tenantId, HttpEntity<Employee> request) {
+        return restTemplate.exchange(employeePathURI + "update", HttpMethod.PUT, request, Employee.class, tenantId);
+    }
+
+    private ResponseEntity<Skill> addSkill(Integer tenantId, Skill skill) {
+        return restTemplate.postForEntity(skillPathURI + "add", skill, Skill.class, tenantId);
+    }
+
+    private void deleteSkill(Integer tenantId, Long id) {
+        restTemplate.delete(skillPathURI + id, tenantId);
+    }
+
+    private ResponseEntity<Contract> addContract(Integer tenantId, Contract contract) {
+        return restTemplate.postForEntity(contractPathURI + "add", contract, Contract.class, tenantId);
+    }
+
+    private void deleteContract(Integer tenantId, Long id) {
+        restTemplate.delete(contractPathURI + id, tenantId);
+    }
+
+    @Test
+    public void getEmployeeListTest() {
+        Integer tenantId = 1;
+        Integer tenantId2 = 2;
+        String name = "name";
+        String name2 = "name2";
+
+        ResponseEntity<Skill> skillResponseA = addSkill(tenantId, new Skill(tenantId, "A"));
+        ResponseEntity<Skill> skillResponseB = addSkill(tenantId, new Skill(tenantId, "B"));
+        ResponseEntity<Skill> skillResponseC = addSkill(tenantId2, new Skill(tenantId2, "C"));
+
+        Skill skillA = skillResponseA.getBody();
+        Skill skillB = skillResponseB.getBody();
+        Skill skillC = skillResponseC.getBody();
+
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Set<Skill> testSkillSet2 = new HashSet<>();
+        testSkillSet2.add(skillC);
+
+        ResponseEntity<Contract> contractResponseA = addContract(tenantId, new Contract(tenantId, "A"));
+        ResponseEntity<Contract> contractResponseB = addContract(tenantId2, new Contract(tenantId2, "B"));
+
+        Contract contractA = contractResponseA.getBody();
+        Contract contractB = contractResponseB.getBody();
+
+        Employee employee = new Employee(tenantId, name, contractA, testSkillSet);
+        Employee employee2 = new Employee(tenantId, name2, contractA, testSkillSet);
+        Employee employee3 = new Employee(tenantId2, name, contractB, testSkillSet2);
+
+        ResponseEntity<Employee> postResponse = addEmployee(tenantId, employee);
+        ResponseEntity<Employee> postResponse2 = addEmployee(tenantId, employee2);
+        ResponseEntity<Employee> postResponse3 = addEmployee(tenantId2, employee3);
+
+        ResponseEntity<List<Employee>> response = getEmployees(tenantId);
+        ResponseEntity<List<Employee>> response2 = getEmployees(tenantId2);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains(postResponse.getBody());
+        assertThat(response.getBody()).contains(postResponse2.getBody());
+
+        assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response2.getBody()).contains(postResponse3.getBody());
+
+        deleteEmployee(tenantId, postResponse.getBody().getId());
+        deleteEmployee(tenantId, postResponse2.getBody().getId());
+        deleteEmployee(tenantId2, postResponse3.getBody().getId());
+
+        deleteSkill(tenantId, skillA.getId());
+        deleteSkill(tenantId, skillB.getId());
+        deleteSkill(tenantId2, skillC.getId());
+
+        deleteContract(tenantId, contractA.getId());
+        deleteContract(tenantId2, contractB.getId());
+    }
+
+    @Test
+    public void getAndCreateEmployeeTest() {
+        Integer tenantId = 1;
+        String name = "name";
+
+        ResponseEntity<Skill> skillResponseA = addSkill(tenantId, new Skill(tenantId, "A"));
+        ResponseEntity<Skill> skillResponseB = addSkill(tenantId, new Skill(tenantId, "B"));
+
+        Skill skillA = skillResponseA.getBody();
+        Skill skillB = skillResponseB.getBody();
+
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        ResponseEntity<Contract> contractResponseEntity = addContract(tenantId, new Contract(tenantId, "A"));
+        Contract contractA = contractResponseEntity.getBody();
+
+        Employee employee = new Employee(tenantId, name, contractA, testSkillSet);
+        ResponseEntity<Employee> postResponse = addEmployee(tenantId, employee);
+
+        ResponseEntity<Employee> response = getEmployee(tenantId, postResponse.getBody().getId());
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(postResponse.getBody());
+
+        deleteEmployee(tenantId, postResponse.getBody().getId());
+        deleteSkill(tenantId, skillA.getId());
+        deleteSkill(tenantId, skillB.getId());
+        deleteContract(tenantId, contractA.getId());
+    }
+
+    @Test
+    public void deleteEmployeeTest() {
+        Integer tenantId = 1;
+        String name = "name";
+
+        ResponseEntity<Skill> skillResponseA = addSkill(tenantId, new Skill(tenantId, "A"));
+        ResponseEntity<Skill> skillResponseB = addSkill(tenantId, new Skill(tenantId, "B"));
+
+        Skill skillA = skillResponseA.getBody();
+        Skill skillB = skillResponseB.getBody();
+
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        ResponseEntity<Contract> contractResponseEntity = addContract(tenantId, new Contract(tenantId, "A"));
+        Contract contractA = contractResponseEntity.getBody();
+
+        Employee employee = new Employee(tenantId, name, contractA, testSkillSet);
+        ResponseEntity<Employee> postResponse = addEmployee(tenantId, employee);
+
+        deleteEmployee(tenantId, postResponse.getBody().getId());
+
+        ResponseEntity<List<Employee>> response = getEmployees(tenantId);
+
+        assertThat(response.getBody()).isEmpty();
+
+        deleteSkill(tenantId, skillA.getId());
+        deleteSkill(tenantId, skillB.getId());
+        deleteContract(tenantId, contractA.getId());
+    }
+
+    @Test
+    public void updateEmployeeTest() {
+        Integer tenantId = 1;
+        String name = "name";
+
+        ResponseEntity<Skill> skillResponseA = addSkill(tenantId, new Skill(tenantId, "A"));
+        ResponseEntity<Skill> skillResponseB = addSkill(tenantId, new Skill(tenantId, "B"));
+
+        Skill skillA = skillResponseA.getBody();
+        Skill skillB = skillResponseB.getBody();
+
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        ResponseEntity<Contract> contractResponseEntity = addContract(tenantId, new Contract(tenantId, "A"));
+        Contract contractA = contractResponseEntity.getBody();
+
+        Employee employee = new Employee(tenantId, name, contractA, Collections.emptySet());
+
+        ResponseEntity<Employee> postResponse = addEmployee(tenantId, employee);
+
+        Employee employee2 = new Employee(tenantId, "name2", contractA, testSkillSet);
+        employee2.setId(postResponse.getBody().getId());
+        HttpEntity<Employee> request = new HttpEntity<>(employee2);
+
+        ResponseEntity<Employee> putResponse = updateEmployee(tenantId, request);
+
+        ResponseEntity<Employee> response = getEmployee(tenantId, putResponse.getBody().getId());
+
+        assertThat(putResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(putResponse.getBody()).isEqualTo(response.getBody());
+
+        deleteEmployee(tenantId, putResponse.getBody().getId());
+    }
+}
---
+++ employee-rostering-backend/src/test/java/org/optaweb/employeerostering/employee/EmployeeServiceTest.java
@@ -0,0 +1,376 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.employeerostering.employee;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.optaweb.employeerostering.domain.contract.Contract;
+import org.optaweb.employeerostering.domain.employee.Employee;
+import org.optaweb.employeerostering.domain.skill.Skill;
+import org.optaweb.employeerostering.service.ContractService;
+import org.optaweb.employeerostering.service.EmployeeService;
+import org.optaweb.employeerostering.service.SkillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.util.NestedServletException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class EmployeeServiceTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceTest.class);
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private SkillService skillService;
+
+    @Autowired
+    private ContractService contractService;
+
+    private Skill createSkill(Integer tenantId, String name) {
+        Skill skill = new Skill(tenantId, name);
+        return skillService.createSkill(tenantId, skill);
+    }
+
+    private Contract createContract(Integer tenantId, String name) {
+        Contract contract = new Contract(tenantId, name);
+        return contractService.createContract(tenantId, contract);
+    }
+
+    @Test
+    public void getEmployeeListTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                            .get("/rest/tenant/{tenantId}/employee", 1)
+                            .accept(MediaType.APPLICATION_JSON))
+                .andDo(mvcResult -> logger.info(mvcResult.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+
+        employeeService.createEmployee(tenantId, employee);
+
+        mvc.perform(MockMvcRequestBuilders
+                            .get("/rest/tenant/{tenantId}/employee/{id}", tenantId, employee.getId())
+                            .accept(MediaType.APPLICATION_JSON))
+                .andDo(mvcResult -> logger.info(mvcResult.toString()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.tenantId").value(tenantId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.contract").value(contract))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.skillProficiencySet").isNotEmpty());
+    }
+
+    @Test
+    public void getNonExistentEmployeeTest() {
+        Integer tenantId = 1;
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .get("/rest/tenant/{tenantId}/employee/{id}", tenantId, -1L)))
+                .withMessage("Request processing failed; nested exception is javax.persistence.EntityNotFound" +
+                                     "Exception: No Employee entity found with ID (-1).");
+    }
+
+    @Test
+    public void getNonMatchingEmployeeTest() {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+
+        employeeService.createEmployee(tenantId, employee);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .get("/rest/tenant/{tenantId}/employee/{id}", 2,
+                                                           employee.getId())))
+                .withMessage("Request processing failed; nested exception is java.lang.IllegalStateException: The " +
+                                     "tenantId (2) does not match the persistable (name)'s tenantId (1).");
+    }
+
+    @Test
+    public void deleteEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+
+        employeeService.createEmployee(tenantId, employee);
+
+        mvc.perform(MockMvcRequestBuilders
+                            .delete("/rest/tenant/{tenantId}/employee/{id}", tenantId, employee.getId())
+                            .accept(MediaType.APPLICATION_JSON))
+                .andDo(mvcResult -> logger.info(mvcResult.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteNonExistentEmployeeTest() {
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .delete("/rest/tenant/{tenantId}/employee/{id}", 1, -1L)))
+                .withMessage("Request processing failed; nested exception is javax.persistence.EntityNotFound" +
+                                     "Exception: No Employee entity found with ID (-1).");
+    }
+
+    @Test
+    public void deleteNonMatchingEmployeeTest() {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+
+        employeeService.createEmployee(tenantId, employee);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders.delete("/rest/tenant/{tenantId}/employee/{id}", 2,
+                                                                            employee.getId())))
+                .withMessage("Request processing failed; nested exception is java.lang.IllegalStateException: " +
+                                     "The tenantId (2) does not match the persistable (name)'s tenantId (1).");
+    }
+
+    @Test
+    public void createEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+        String body = (new ObjectMapper()).writeValueAsString(employee);
+
+        mvc.perform(MockMvcRequestBuilders
+                            .post("/rest/tenant/{tenantId}/employee/add", tenantId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(body)
+                            .accept(MediaType.APPLICATION_JSON))
+                .andDo(mvcResult -> logger.info(mvcResult.toString()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.tenantId").value(tenantId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.contract").value(contract))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.skillProficiencySet").isNotEmpty());
+    }
+
+    @Test
+    public void createNonMatchingEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, testSkillSet);
+        String body = (new ObjectMapper()).writeValueAsString(employee);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .post("/rest/tenant/{tenantId}/employee/add", 2)
+                                                      .contentType(MediaType.APPLICATION_JSON)
+                                                      .content(body)))
+                .withMessage("Request processing failed; nested exception is java.lang.IllegalStateException: " +
+                                     "The tenantId (2) does not match the persistable (name)'s tenantId (1).");
+    }
+
+    @Test
+    public void updateEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, Collections.emptySet());
+
+        employeeService.createEmployee(tenantId, employee);
+
+        Employee employee2 = new Employee(tenantId, "name2", contract, testSkillSet);
+        employee2.setId(employee.getId());
+        String body = (new ObjectMapper()).writeValueAsString(employee2);
+
+        mvc.perform(MockMvcRequestBuilders
+                            .put("/rest/tenant/{tenantId}/employee/update", tenantId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(body)
+                            .accept(MediaType.APPLICATION_JSON))
+                .andDo(mvcResult -> logger.info(mvcResult.toString()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.tenantId").value(tenantId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("name2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.contract").value(contract))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.skillProficiencySet").isNotEmpty());
+    }
+
+    @Test
+    public void updateNonMatchingEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId, "A");
+        Skill skillB = createSkill(tenantId, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+
+        Employee employee = new Employee(tenantId, name, contract, Collections.emptySet());
+
+        employeeService.createEmployee(tenantId, employee);
+
+        Employee employee2 = new Employee(tenantId, "name2", contract, testSkillSet);
+        String body = (new ObjectMapper()).writeValueAsString(employee2);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .put("/rest/tenant/{tenantId}/employee/update", 2)
+                                                      .contentType(MediaType.APPLICATION_JSON)
+                                                      .content(body)))
+                .withMessage("Request processing failed; nested exception is java.lang.IllegalStateException: " +
+                                     "The tenantId (2) does not match the persistable (name2)'s tenantId (1).");
+    }
+
+    @Test
+    public void updateNonExistentEmployeeTest() throws Exception {
+        Contract contract = new Contract();
+
+        Employee employee = new Employee(1, "name", contract, Collections.emptySet());
+        employee.setId(-1L);
+        String body = (new ObjectMapper()).writeValueAsString(employee);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .put("/rest/tenant/{tenantId}/employee/update", 1)
+                                                      .contentType(MediaType.APPLICATION_JSON)
+                                                      .content(body)))
+                .withMessage("Request processing failed; nested exception is javax.persistence.EntityNotFound" +
+                                     "Exception: Employee entity with ID (-1) not found.");
+    }
+
+    @Test
+    public void updateChangeTenantIdEmployeeTest() throws Exception {
+        Integer tenantId = 1;
+        Integer tenantId2 = 2;
+        String name = "name";
+
+        Skill skillA = createSkill(tenantId2, "A");
+        Skill skillB = createSkill(tenantId2, "B");
+        Set<Skill> testSkillSet = new HashSet<>();
+        testSkillSet.add(skillA);
+        testSkillSet.add(skillB);
+
+        Contract contract = createContract(tenantId, name);
+        Contract contract2 = createContract(tenantId2, name);
+
+        Employee employee = new Employee(tenantId, name, contract, Collections.emptySet());
+
+        employeeService.createEmployee(tenantId, employee);
+
+        Employee employee2 = new Employee(2, name, contract2, testSkillSet);
+        employee2.setId(employee.getId());
+        String body = (new ObjectMapper()).writeValueAsString(employee2);
+
+        assertThatExceptionOfType(NestedServletException.class)
+                .isThrownBy(() -> mvc.perform(MockMvcRequestBuilders
+                                                      .put("/rest/tenant/{tenantId}/employee/update", 2)
+                                                      .contentType(MediaType.APPLICATION_JSON)
+                                                      .content(body)))
+                .withMessage("Request processing failed; nested exception is java.lang.IllegalState" +
+                                     "Exception: Employee entity with tenantId (1) cannot change tenants.");
+    }
+}
