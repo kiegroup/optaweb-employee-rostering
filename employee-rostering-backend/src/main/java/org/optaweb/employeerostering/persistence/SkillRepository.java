89c7068a34cc8e3ec4e64495712b7855e7fbae91
Julian Cui <jucui@redhat.com>
2019-07-23 14:52:51 -0400

    Add query annotation to Skill and Spot repository

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/SkillRepository.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/SkillRepository.java
@@ -20,9 +20,14 @@ import java.util.List;
 
 import org.optaweb.employeerostering.domain.skill.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+    @Query("select s from Skill s " +
+            "where s.tenantId = :tenantId " +
+            "order by LOWER(s.name)")
     List<Skill> findAllByTenantId(Integer tenantId);
 }
