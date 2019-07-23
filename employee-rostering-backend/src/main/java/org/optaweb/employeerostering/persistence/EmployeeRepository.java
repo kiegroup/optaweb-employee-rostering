1b578484bf58c3de2d653063686f1940c7b5df8f
Julian Cui <jucui@redhat.com>
2019-07-23 15:02:44 -0400

    Add query annotation to EmployeeRepository

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/EmployeeRepository.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/persistence/EmployeeRepository.java
@@ -20,9 +20,14 @@ import java.util.List;
 
 import org.optaweb.employeerostering.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("select e from Employee e " +
+            "where e.tenantId = :tenantId " +
+            "order by LOWER(e.name)")
     List<Employee> findAllByTenantId(Integer tenantId);
 }
