7235f48ee4e6338f490a20a4ea72eff5898abd9d
Christopher Chianelli <chris.chianelli@gmail.com>
2019-08-01 14:23:37 -0400

    Fix broken tests caused by adding availability

--- employee-rostering-frontend/src/store/alert/alert.test.ts
+++ employee-rostering-frontend/src/store/alert/alert.test.ts
@@ -179,6 +179,10 @@ const state: AppState = {
     isLoading: true,
     shiftRosterView: null
   },
+  availabilityRoster: {
+    isLoading: true,
+    availabilityRosterView: null
+  },
   solverState: {
     isSolving: false
   },
--- employee-rostering-frontend/src/store/shift/shift.test.ts
+++ employee-rostering-frontend/src/store/shift/shift.test.ts
@@ -349,6 +349,10 @@ const state: AppState = {
     isLoading: true,
     shiftRosterView: null
   },
+  availabilityRoster: {
+    isLoading: true,
+    availabilityRosterView: null
+  },
   solverState: {
     isSolving: false
   },
