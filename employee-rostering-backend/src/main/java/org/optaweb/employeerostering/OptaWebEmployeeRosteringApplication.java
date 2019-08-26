2e3b40bb451e5c7bd71450b22755c8d741be830f
Julian Cui <jucui@redhat.com>
2019-08-26 11:06:36 -0400

    Add Swagger2 to generate API docs

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/OptaWebEmployeeRosteringApplication.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/OptaWebEmployeeRosteringApplication.java
@@ -21,8 +21,10 @@ import org.optaplanner.persistence.jackson.api.OptaPlannerJacksonModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OptaWebEmployeeRosteringApplication {
 
     @Bean
