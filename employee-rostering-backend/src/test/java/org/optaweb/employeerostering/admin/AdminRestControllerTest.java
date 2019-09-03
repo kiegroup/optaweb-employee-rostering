b783baa91818b713ee2733aa653e4d978dbf793f
Julian Cui <jucui@redhat.com>
2019-09-03 15:16:35 -0400

    Add reset application test

---
+++ employee-rostering-backend/src/test/java/org/optaweb/employeerostering/admin/AdminRestControllerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaweb.employeerostering.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AdminRestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private final String adminPathURI = "http://localhost:8080/rest/admin/";
+
+    private ResponseEntity<Void> resetApplication() {
+        return restTemplate.postForEntity(adminPathURI + "reset", null, Void.class);
+    }
+
+    @Test
+    public void resetApplicationTest() {
+        ResponseEntity<Void> resetResponse = resetApplication();
+        assertThat(resetResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
