77f800aa30fe9b6fb15c77f83697e1c0ba430df8
Julian Cui <jucui@redhat.com>
2019-09-23 10:26:57 -0400

    Remove unused Logger

--- employee-rostering-backend/src/main/java/org/optaweb/employeerostering/OptaWebExceptionHandler.java
+++ employee-rostering-backend/src/main/java/org/optaweb/employeerostering/OptaWebExceptionHandler.java
@@ -17,8 +17,6 @@
 package org.optaweb.employeerostering;
 
 import org.optaweb.employeerostering.domain.exception.ServerSideExceptionInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,8 +25,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class OptaWebExceptionHandler extends ResponseEntityExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(OptaWebExceptionHandler.class);
 
     private ExceptionDataMapper exceptionDataMapper;
 
