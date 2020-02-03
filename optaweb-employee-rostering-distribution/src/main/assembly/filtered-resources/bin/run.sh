1b9e79c3aab16fdef0dab2355919c88f7cc72eca
Jiří Locker <jiri.locker@gmail.com>
2020-02-03 12:46:23 +0100

    PLANNER-1797: Move PostgreSQL to a production profile

--- optaweb-employee-rostering-distribution/src/main/assembly/filtered-resources/bin/run.sh
+++ optaweb-employee-rostering-distribution/src/main/assembly/filtered-resources/bin/run.sh
@@ -4,5 +4,4 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 
 java -jar ${script_dir}/employee-rostering-standalone-${project.version}-exec.jar \
 --server.address=localhost \
---server.port=8080 \
---spring.profiles.active=dev
+--server.port=8080
